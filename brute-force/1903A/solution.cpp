3937e964bd614bca190ae880f996b51d7aa4ac7a
deepak chaudhary <deepak.240101266@iiitbh.ac.in>
2026-07-09 17:30:24 +0530

    Add solution: Halloumi Boxes (1903A)

---
+++ brute-force/1903A/solution.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        vector<int> v(n);
+        for (int i = 0; i < n; i++) {
+            cin >> v[i];
+        }
+
+        if (is_sorted(v.begin(), v.end()) || k > 1) {
+            cout << "YES\n";
+        } else {
+            cout << "NO\n";
+        }
+    }
+    return 0;
+}
