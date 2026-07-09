ec0b39dfd1d8258771bca8923e98f89185d0ceea
deepak chaudhary <deepak.240101266@iiitbh.ac.in>
2026-07-09 17:30:32 +0530

    Add solution: Team (231A)

---
+++ brute-force/231A/solution.cpp
@@ -0,0 +1,14 @@
+# include <bits/stdc++.h>
+using namespace std;
+int main() {
+   int t=0 ;
+   int k ;
+   cin>> k;
+   while(k--){
+    int a,b,c ;
+    cin>>a>>b>>c ;
+    if((a+b+c)>1) t++ ;
+
+   }
+   cout<<t<<endl ;
+ }
