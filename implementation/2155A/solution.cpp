fa7f1d21c3d2ea4d85252ea2a4b489b619d3de0c
deepak chaudhary <deepak.240101266@iiitbh.ac.in>
2026-07-09 17:15:44 +0530

    Add solution: El fucho (2155A)

---
+++ implementation/2155A/solution.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t ;
+	cin>>t ;
+	while(t--){
+	    int a ;
+	    cin>>a ;
+	    
+	   cout<<2*a-2<<endl ;
+	}
+
+}
