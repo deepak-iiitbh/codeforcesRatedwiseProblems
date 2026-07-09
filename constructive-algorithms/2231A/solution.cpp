d1b6eda83a9f12704a3a393a4507f59aaa7dfb06
deepak chaudhary <deepak.240101266@iiitbh.ac.in>
2026-07-09 16:15:02 +0530

    Add solution: Construct an Array (2231A)

---
+++ constructive-algorithms/2231A/solution.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define endl '\n'
+
+const int MOD = 1e9 + 7;
+const int INF = LLONG_MAX >> 1;
+
+void solve() {
+    int a ;
+    cin>>a ;
+    int idx = 1 ;
+    while(a--){
+        cout<<idx<<" " ;
+        idx+=2 ;
+    }
+    cout<<endl ;
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int tc = 1;
+    cin >> tc;
+    while(tc--) solve();
+}
