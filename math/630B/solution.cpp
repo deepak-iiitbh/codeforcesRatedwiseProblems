431c9a4c2fe857a5546e401750474cead9221abc
deepak chaudhary <deepak.240101266@iiitbh.ac.in>
2026-07-09 16:30:08 +0530

    Add solution: Moore's Law (630B)

---
+++ math/630B/solution.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define int long long
+
+const int MOD = 1e9 + 7;
+const int INF = LLONG_MAX >> 1;
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    double a, b ;
+    cin>>a>>b ;
+    cout<<fixed << setprecision(6)<<a * pow(1.000000011 , b)<<endl ;
+
+    return 0;
+}
