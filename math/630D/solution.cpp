f8aa50fa40f6bd137c7e6197f7636b9a9818f30f
deepak chaudhary <deepak.240101266@iiitbh.ac.in>
2026-07-09 16:29:51 +0530

    Add solution: Hexagons! (630D)

---
+++ math/630D/solution.cpp
@@ -0,0 +1,20 @@
+    #include <bits/stdc++.h>
+    using namespace std;
+
+    #define endl '\n'
+    #define int long long
+
+    const int MOD = 1e9 + 7;
+    const int INF = LLONG_MAX >> 1;
+
+    signed main() {
+        ios::sync_with_stdio(false);
+        cin.tie(NULL);
+
+        int a ; 
+        cin>>a ; 
+        int result = 6 * ((a)*(a+1)/2) ;
+        cout<<++result<<endl ;
+
+        return 0;
+    }
